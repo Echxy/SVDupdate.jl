974e91a77f0deead7af2e3de2dd3e4fd78e29e0a
Diego Echeverria <diegoecheverria84@gmail.com>
2025-02-07 11:07:07 -0300

    Updated project

--- src/SVDupdate.jl
+++ src/SVDupdate.jl
@@ -1,5 +1,14 @@
 module SVDupdate
 
-# Write your package code here.
+using LinearAlgebra
+using TimerOutputs
+using BenchmarkTools
+using Plots
+
+include("SVDfunctions.jl")
+include("Plotters.jl")
+
+export benchmark_time_M, benchmark_time_N, benchmark_alloc_M, benchmark_alloc_N, plot_results
+export UpdateISVD, UpdateISVD_memory
 
 end
